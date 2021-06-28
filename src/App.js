efd835d04f8d86ba2d87364ac26fdbcd8befcc05
Arcywojak <maciej778@op.pl>
2021-06-28 21:47:16 +0200

    add simple styles to footer and add full page container into app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import HeaderComponent from './constant-components/header/header.component';
 import FooterComponent from './constant-components/footer/footer.component';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent/>
+        <Container className="full-height-container">
 
+        </Container>
       <FooterComponent/>
     </div>
   );
--- src/constant-components/footer/footer.component.js
+++ src/constant-components/footer/footer.component.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles((theme) => {
+const useStyles = makeStyles((theme) => ({
     footer: {
-
+        backgroundColor: theme.palette.primary.dark,
+        color: theme.palette.primary.contrastText,
+        padding:"1em"
     }
-})
+}));
 
 const FooterComponent = () => {
     const classes = useStyles();
 
     return (
         <footer className={classes.footer}>
-            footer
+            <Typography variant="h6" className={classes.title}>
+                Footer
+            </Typography>
         </footer>
     )
 }
