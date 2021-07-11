ce30741d996d8d404a495f30da1fa7ceabd6982b
maciej778 <maciej778TEST@gmail.com>
2021-07-11 09:52:45 +0200

    change the order and time of executing of animations in home-page

--- src/pages/home-page/home.page.js
+++ src/pages/home-page/home.page.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import SearchInput from '../../components/home-page-components/search-input';
 import Typography from '@material-ui/core/Typography';
 import WeatherBlock from '../../components/home-page-components/weather-block';
@@ -8,7 +9,16 @@ import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { NO_ANIMATION, HIDING_ANIMATION, SHOW_ANIMATION } from '../../constants/animations';
 
+const useStyles = makeStyles({
+  loaderWrapper: {
+      marginTop: "calc(12px + 1vw)"
+  },
+});
+
+
 const HomePage = () => {
+
+    const classes = useStyles();
 
     const [inputValue, setInputValue] = useState('');
     const [weather, setWeather] = useState(null);
@@ -22,18 +32,29 @@ const HomePage = () => {
 
       weatherService.getWeatherByCityName(inputValue).then(res => {
         if(!weather) {
-          setAnimationTypeForTitle(HIDING_ANIMATION);
-          setAnimationTypeForWeatherBlock(SHOW_ANIMATION);
+
+          setTimeout(() => {
+            setWeather(res.data);
+            setAnimationTypeForWeatherBlock(SHOW_ANIMATION);
+          }, 2500)
+
+          setTimeout(() => {
+            setAnimationTypeForTitle(HIDING_ANIMATION);
+            setIsLoading(false);
+          }, 1500)
+        } else {
+          setTimeout(() => {
+            setIsLoading(false);
+            setWeather(res.data);
+          }, 1500)
         }
-        setWeather(res.data);
-        setIsLoading(false);
+        
       })
     }
 
     const weatherBlock = weather != null ? <WeatherBlock hidden={true} weather = {weather} /> : null;
 
-    const loader = isLoading ?  <div ><CircularProgress /></div> : null;
-
+    const loader = isLoading ?  <div className={classes.loaderWrapper}><CircularProgress /></div> : null;
 
     return (
         <>
